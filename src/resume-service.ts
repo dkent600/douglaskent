9c775fefb1c578268a4b77b9fcb2271e19532e3a
Doug Kent <github@douglaskent.com>
2023-04-03 18:10:56 -0600

    tsconfig

--- src/resume-service.ts
+++ src/resume-service.ts
@@ -8,9 +8,7 @@ export type IResumeService = ResumeService;
 export const IResumeService = DI.createInterface<IResumeService>();
 
 export class ResumeService {
-
     public static register(container: IContainer) {
         Registration.singleton(IResumeService, ResumeService).register(container);
     }
-
 }
