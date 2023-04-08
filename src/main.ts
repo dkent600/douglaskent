4fa68eabb6737a3401fd0cd4d99c84241da04a0d
Doug Kent <github@douglaskent.com>
2023-04-08 09:09:28 -0600

    make sanitize keep anchor target attributes

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,12 @@ import "node-waves";
 import "bootstrap-material-design";
 
 import { Resume } from "./pages/resume/resume";
+import { initializeMarkdown } from "./resources/value-converters/sanitizeHtml";
 import { ResumeService } from "./services/resume-service";
 import { ResumeStore } from "./stores/resume-store";
 import * as resources from "./resources";
+
+initializeMarkdown();
 
 // eslint-disable-next-line prettier/prettier
 void Aurelia
--- src/resources/value-converters/sanitizeHtml.ts
+++ src/resources/value-converters/sanitizeHtml.ts
@@ -1,6 +1,39 @@
 import { valueConverter } from "aurelia";
 
-import { sanitize } from "dompurify";
+import { addHook, sanitize } from "dompurify";
+
+export const initializeMarkdown = () => {
+  // For temporarily saving original target value
+  const TEMP_TARGET_ATTRIBUTE = "data-target-temp";
+
+  addHook("beforeSanitizeAttributes", function (node) {
+    let targetValue;
+    // Preserve default target attribute value
+    if (node.tagName === "A") {
+      targetValue = node.getAttribute("target");
+
+      if (targetValue) {
+        node.setAttribute(TEMP_TARGET_ATTRIBUTE, targetValue);
+      }
+      // else {
+      //   // set default value
+      //   node.setAttribute("target", "_self");
+      // }
+    }
+  });
+
+  addHook("afterSanitizeAttributes", function (node) {
+    if (node.tagName === "A" && node.hasAttribute(TEMP_TARGET_ATTRIBUTE)) {
+      node.setAttribute("target", node.getAttribute(TEMP_TARGET_ATTRIBUTE) ?? "");
+      node.removeAttribute(TEMP_TARGET_ATTRIBUTE);
+
+      // set `rel="noopener"` to prevent another security issue.
+      if (node.getAttribute("target") === "_blank") {
+        node.setAttribute("rel", "noopener noreferrer");
+      }
+    }
+  });
+};
 
 /**
  * Html Sanitizer to prevent script injection.
