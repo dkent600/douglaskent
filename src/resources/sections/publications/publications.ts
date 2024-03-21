e9ad72d7fe74db1dddb1b55cc849ae3468eec85b
Doug Kent <github@douglaskent.com>
2024-03-20 19:46:29 -0600

    fix show more/less publications link

--- src/resources/sections/publications/publications.ts
+++ src/resources/sections/publications/publications.ts
@@ -13,12 +13,7 @@ export class Publications {
     this.publications = this.resumeStore.publications;
   }
 
-  attaching() {
-    $("#publications-list").on("show.bs.collapse", () => {
-      this.showingPublications = true;
-    });
-    $("#publications-list").on("hide.bs.collapse", () => {
-      this.showingPublications = false;
-    });
+  toggleshowingPublications() {
+    this.showingPublications = !this.showingPublications;
   }
 }
