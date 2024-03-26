6b76e570c7e3e06cf7cf88d259ca9f4731df67f8
Doug Kent <github@douglaskent.com>
2024-03-26 09:39:16 -0600

    add single testimonial in short resume

--- src/resources/sections/testimonials/testimonials.ts
+++ src/resources/sections/testimonials/testimonials.ts
@@ -1,4 +1,4 @@
-import { customElement } from "aurelia";
+import { bindable, customElement } from "aurelia";
 
 import { IResumeStore, ITestimonial } from "../../../stores/resume-store";
 
@@ -7,6 +7,7 @@ import template from "./testimonials.html";
 @customElement({ name: "testimonials", template })
 export class Testimonials {
   testimonials!: Array<ITestimonial>;
+  @bindable justOne = false;
   constructor(@IResumeStore private readonly resumeStore: IResumeStore) {
     this.testimonials = this.resumeStore.testimonials;
   }
