5e427b00eb47f417cff48da26d0b823cc1c0723e
Doug Kent <github@douglaskent.com>
2024-03-27 17:56:42 -0600

    shrink the travel and education sections

--- src/resources/sections/travel/travel.ts
+++ src/resources/sections/travel/travel.ts
@@ -1,15 +1,17 @@
 import { bindable, customElement } from "aurelia";
 
-import { ICitizenship, ILanguage, IResumeStore } from "../../../stores/resume-store";
+import { IBasics, ICitizenship, ILanguage, IResumeStore } from "../../../stores/resume-store";
 
 import template from "./travel.html";
 
 @customElement({ name: "travel", template })
 export class Travel {
   citizenship!: Array<ICitizenship>;
+  basics!: IBasics;
   languages!: Array<ILanguage>;
   constructor(@IResumeStore private readonly resumeStore: IResumeStore) {
     this.citizenship = this.resumeStore.citizenship;
     this.languages = this.resumeStore.languages;
+    this.basics = this.resumeStore.basics;
   }
 }
