1d5f163d989d79f6cbc00de6343bf837b82023a6
dkent600 <github@douglaskent.com>
2024-08-13 09:45:01 -0600

    update aurelia version

--- src/resource.d.ts
+++ src/resource.d.ts
@@ -1,11 +1,11 @@
 declare module "*.html" {
-  import { IContainer, PartialBindableDefinition } from "aurelia";
+  import { IContainer } from "aurelia";
   export const name: string;
   export const template: string;
   export default template;
   export const dependencies: string[];
   export const containerless: boolean | undefined;
-  export const bindables: (string | (PartialBindableDefinition & { name: string }))[];
+  export const bindables: Record<string, Partial<BindableDefinition>>;
   export const shadowOptions: { mode: "open" | "closed" } | undefined;
   export function register(container: IContainer): void;
 }
