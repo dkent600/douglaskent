a7091aef7acf9c16bc4f52f6ae5ac10f33cbf66e
dkent600 <github@douglaskent.com>
2026-08-29 12:14:51 -0600

    filter and reorder the accomplishments

--- src/pages/resume/sections/accomplishments/accomplishments.ts
+++ src/pages/resume/sections/accomplishments/accomplishments.ts
@@ -1,10 +1,14 @@
 import { customElement, resolve } from "aurelia";
-
 import { IAccomplishment, IResumeStore } from "../../../../stores/resume-store";
+import { WhichResumeOnly } from "../../../../resources/attributes/whichResumeOnly";
 
 import template from "./accomplishments.html";
 @customElement({ name: "accomplishments", template })
 export class Accomplishments {
   readonly resumeStore = resolve(IResumeStore);
-  readonly accomplishments: Array<IAccomplishment> = this.resumeStore.accomplishments;
+  private readonly all: Array<IAccomplishment> = this.resumeStore.accomplishments;
+
+  get accomplishments(): Array<IAccomplishment> {
+    return WhichResumeOnly.isShort ? this.all.filter((a) => a.showOnShort) : this.all;
+  }
 }
